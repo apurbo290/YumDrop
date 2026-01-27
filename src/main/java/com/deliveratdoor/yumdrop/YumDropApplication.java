10910372e06b1cbb2331a993f9d91c1f30150711
Apurba Bhattacharya <apurbo.bhattacharya@yahoo.com>
2026-01-27 20:29:40 +0530

    Initiate YumDrop Dev

---
+++ src/main/java/com/deliveratdoor/yumdrop/YumDropApplication.java
@@ -0,0 +1,13 @@
+package com.deliveratdoor.yumdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumDropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YumDropApplication.class, args);
+    }
+
+}
